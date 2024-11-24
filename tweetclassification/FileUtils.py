62a9dca9db7586f09b2c5d054304ed308d7a75f4
Felix Neutatz <neutatz@googlemail.com>
2024-11-24 12:04:18 +0100

    fix csv reader1

--- tweetclassification/FileUtils.py
+++ tweetclassification/FileUtils.py
@@ -32,25 +32,28 @@ def readConversations(file_path):
     conversations = []
 
     for tweet_id in starting_tweets:
-        conversation = Conversation()
-        tweet_raw_id = id2tweet[tweet_id]
-        if "author_id" in df.columns:
-            user = matrix[tweet_raw_id, list(df.columns).index("author_id")]
-        else:
-            user = ""
-
-        text = matrix[tweet_raw_id, list(df.columns).index("text")]
-        conversation.add_tweet(Tweet(user, text, tweet_id))
-        current_id = tweet_id
-        while current_id in maptweets:
-            current_id = maptweets[current_id]
-            tweet_raw_id = id2tweet[current_id]
+        try:
+            conversation = Conversation()
+            tweet_raw_id = id2tweet[tweet_id]
             if "author_id" in df.columns:
                 user = matrix[tweet_raw_id, list(df.columns).index("author_id")]
             else:
                 user = ""
+
             text = matrix[tweet_raw_id, list(df.columns).index("text")]
-            conversation.add_tweet(Tweet(user, text, current_id))
-        conversations.append(conversation)
+            conversation.add_tweet(Tweet(user, text, tweet_id))
+            current_id = tweet_id
+            while current_id in maptweets:
+                current_id = maptweets[current_id]
+                tweet_raw_id = id2tweet[current_id]
+                if "author_id" in df.columns:
+                    user = matrix[tweet_raw_id, list(df.columns).index("author_id")]
+                else:
+                    user = ""
+                text = matrix[tweet_raw_id, list(df.columns).index("text")]
+                conversation.add_tweet(Tweet(user, text, current_id))
+            conversations.append(conversation)
+        except:
+            pass
 
     return conversations
